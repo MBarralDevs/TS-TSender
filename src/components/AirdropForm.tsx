130292834705697287f49215edca0090c2f526f7
mbarraldevs <mbarraldevs@outlook.com>
2025-10-24 13:31:15 +0200

    adding form

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -6,6 +6,7 @@ import { useState } from "react"
 export default function AirdropForm() {
       const [tokenAddress, setTokenAddress] = useState("")
       const [recipients, setRecipients] = useState("")
+      const [amounts, setAmounts] = useState("")
 
       return (
       <div>
@@ -20,6 +21,14 @@ export default function AirdropForm() {
                   placeholder="0x1234...abcd"
                   value={recipients}
                   onChange={(e) => setRecipients(e.target.value)}
+                  large={true}
+            />
+            <InputField
+                  label="Amounts to Airdrop"
+                  placeholder="100\n200\n300"
+                  value={amounts}
+                  onChange={(e) => setAmounts(e.target.value)}
+                  large={true}
             />
       </div>
       )           
