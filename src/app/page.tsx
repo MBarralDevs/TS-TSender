073553a475b725bbb72fb3dc38aa8207e3f8caf3
mbarraldevs <mbarraldevs@outlook.com>
2025-10-28 20:34:02 +0100

    home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Header from "../components/Header"; 
 import AirdropForm from "../components/AirdropForm";
+import HomeContent from "../components/HomeContent";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <AirdropForm />
+      <HomeContent />
     </div>
   );
 }
