fc9ac0f2b505de2c051c7db2a1c96841a7bed23d
mbarraldevs <mbarraldevs@outlook.com>
2025-11-01 13:39:18 +0100

    update

--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -1,7 +1,11 @@
-import { test, expect } from '@playwright/test';
+//import { test, expect } from '@playwright/test';
 import basicSetup from '../wallet-setup/basic.setup';
 import { testWithSynpress } from '@synthetixio/synpress'; 
 import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright';
+import { Context } from 'wagmi';
+
+const test = testWithSynpress(metaMaskFixtures(basicSetup));
+const {expect} = test;
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000');
@@ -10,8 +14,11 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/TSender/);
 });
 
-test('Should show form when connected, otherwise not', async ({ page }) => {
+test('Should show form when connected, otherwise not', async ({ page, context, metamaskPage, extensionId }) => {
   await page.goto('/')
   //We check that we see "Please Connect Wallet"
   await expect(page.getByText('Please Connect')).toBeVisible();
+
+  //We create a new Metamask instance
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
 });
